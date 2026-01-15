9e5b32e80da2c362b486f1f900aa0ac11e45f484
K <k.rev.urtica@gmail.com>
2026-01-15 15:49:13 +0900

    fix

--- server/utils/runtimeConfig.ts
+++ server/utils/runtimeConfig.ts
@@ -4,7 +4,7 @@ let runtimeConfigInstance: NitroRuntimeConfig;
 
 export const generateRuntimeConfig = () => ({
   // better-auth
-  betterAuthBaseURL: process.env.NUXT_PUBLIC_SITE_URL,
+  betterAuthBaseURL: process.env.NUXT_PUBLIC_BETTER_AUTH_URL,
   betterAuthSecret: process.env.NUXT_BETTER_AUTH_SECRET,
   // OAuth plugin
   oauthGoogleClientId: process.env.NUXT_OAUTH_GOOGLE_CLIENT_ID,
